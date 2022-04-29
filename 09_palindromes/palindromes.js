e99f10b465e117c279b1d2bd8fcd35c312a5bb8b
Thierry <marcelinthierry@gmail.com>
2022-04-29 19:26:44 +0800

    Add solution to 09_palidromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = (text) => {
+  let newText = text
+    .replace(/[^\w\s]|_/g, "")
+    .replace(/\s+/g, "")
+    .toLowerCase();
+  let start = 0;
+  let end = newText.length - 1;
+  while (start <= end) {
+    if (newText.charAt(start) !== newText.charAt(end)) return false;
+    start++;
+    end--;
+  }
+  return true;
 };
 
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
 // Do not edit below this line
 module.exports = palindromes;
