c2dcee235e79ff3e3037b69f9a3d425a056a4b60
Thierry <marcelinthierry@gmail.com>
2022-04-29 17:49:31 +0800

    Add solution to 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = (text, num) => {
+  let newText = "";
+  if (num < 0) return "ERROR";
+  for (let i = 0; i < num; i++) newText += text;
+  return newText;
 };
 
 // Do not edit below this line
