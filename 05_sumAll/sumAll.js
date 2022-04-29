4fee873812e065a1e7d5bd75a5c5442778824fec
Thierry <marcelinthierry@gmail.com>
2022-04-29 18:44:44 +0800

    Add solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function () {
+  // Let's work with arguments because why not?
+  let num1 = arguments[0];
+  let num2 = arguments[1];
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  )
+    return "ERROR";
 
+  let end = Math.max(num1, num2);
+  let start = Math.min(num1, num2);
+  let sum = 0;
+  for (let i = start; i <= end; i++) sum += i;
+  return sum;
 };
 
 // Do not edit below this line
