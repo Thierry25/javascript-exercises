5a3d2ea5fda8ae737f2371855994074a5d13ba3a
Thierry <marcelinthierry@gmail.com>
2022-04-29 19:10:11 +0800

    Add solution to 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const sum = (arr) => {
+  let sum = 0;
+  arr.forEach((number) => (sum += number));
+  return sum;
 };
 
-const subtract = function() {
-	
+const multiply = (arr) => {
+  let product = 1;
+  arr.forEach((number) => (product *= number));
+  return product;
 };
 
-const sum = function() {
-	
-};
+const power = (base, exponent) => base ** exponent;
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = (number) => {
+  if (number === 0 || number === 1) return 1;
+  if (number < 0) return "ERROR";
+  let factorial = 1;
+  for (let i = 1; i <= number; i++) factorial *= i;
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +31,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
