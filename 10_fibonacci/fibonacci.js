33a0d307cabeb740046a8561e64d2000ed686e34
Thierry <marcelinthierry@gmail.com>
2022-04-29 20:04:40 +0800

    Add solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = (val) => {
+  let a = 0;
+  let b = 1;
+  let newVal = parseInt(val);
+  if (newVal < 0) return "OOPS";
+  for (let i = 1; i <= val; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return a;
 };
-
 // Do not edit below this line
+console.log(fibonacci(25));
 module.exports = fibonacci;
