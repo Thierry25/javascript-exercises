1146646ccd1302a9759505112a27ed29e4109559
Thierry <marcelinthierry@gmail.com>
2022-06-01 12:00:06 +0800

    Update code to follow ES6 guidelines

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,12 +1,11 @@
 // Updated to use ES6 syntax
 
 const getTheTitles = (arr) => {
-  const titles = arr.map((value) => value.title);
+  return arr.map((value) => value.title);
   // let titles = [];
   // for (const obj of arr) {
   //   titles.push(obj["title"]);
   // }
-  return titles;
 };
 
 // Do not edit below this line
